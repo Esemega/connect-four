28cc763c59cc9ca542c033b872ce1a3af931ef96
Esemega <sogaral89@gmail.com>
2020-11-13 19:28:14 +0100

    alterning turn between two players

--- connect4.py
+++ connect4.py
@@ -11,10 +11,13 @@ turn = 0
 while not game_over:
     #Ask for Player 1 Input
     if turn == 0:
-        selection = input("Player 1 - Make your selection (0-6): ")
+        selection = int(input("Player 1 - Make your selection (0-6): "))
 
-        print(selection)
-        print(type(selection))
         
     #Ask for Player 2 Input
+    else:
+        selection = int(input("Player 2 - Make your selection (0-6): "))
+    
+    turn += 1
+    turn = turn % 2
 
