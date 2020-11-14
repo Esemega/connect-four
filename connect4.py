88472798951856e4667dab65d6c472b76a61adab
Esemega <sogaral89@gmail.com>
2020-11-14 14:12:45 +0100

    Graphic logic of the game implemented

--- connect4.py
+++ connect4.py
@@ -1,6 +1,7 @@
 import numpy
 import pygame
 import sys
+import math
 
 NUMBER_OF_ROWS = 6
 NUMBER_OF_COLLUMNS = 7
@@ -11,7 +12,7 @@ SQUARESIZE = 75 #pixels
 SQUARE_OFFSET = 1
 CIRCLE_OFFSET = int(SQUARESIZE/2)
 
-RADIUS = int(SQUARESIZE/2 - 3)
+RADIUS = SQUARESIZE/2 - 3
 BLUE = (0,0,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
@@ -62,7 +63,15 @@ def draw_board(board):
     for col in range(NUMBER_OF_COLLUMNS):
         for row in range(NUMBER_OF_ROWS): 
             pygame.draw.rect(screen, BLUE, (col*SQUARESIZE, row*SQUARESIZE + SQUARE_OFFSET*SQUARESIZE, SQUARESIZE, SQUARESIZE))
-            pygame.draw.circle(screen, BLACK, (col*SQUARESIZE+CIRCLE_OFFSET, row*SQUARESIZE + SQUARE_OFFSET*SQUARESIZE + CIRCLE_OFFSET), RADIUS )
+            pygame.draw.circle(screen, BLACK, (int(col*SQUARESIZE+CIRCLE_OFFSET), int(row*SQUARESIZE + SQUARE_OFFSET*SQUARESIZE + CIRCLE_OFFSET)), RADIUS )
+    
+    for col in range(NUMBER_OF_COLLUMNS):
+        for row in range(NUMBER_OF_ROWS):
+            if board[row][col] == 1:
+                pygame.draw.circle(screen, RED, (int(col*SQUARESIZE+CIRCLE_OFFSET), height - int(row*SQUARESIZE + CIRCLE_OFFSET)), RADIUS )
+            elif board[row][col] == 2:
+                pygame.draw.circle(screen, GREEN, (int(col*SQUARESIZE+CIRCLE_OFFSET), height - int(row*SQUARESIZE + CIRCLE_OFFSET)), RADIUS )
+
 
 
 board = create_board()
@@ -91,7 +100,39 @@ while not game_over:
             sys.exit(0)
         
         if event.type == pygame.MOUSEBUTTONDOWN:
-            continue
+            #GRAPHIC VERSION
+            #Player 1 turn
+            if turn == 0:
+                posX = event.pos[0]
+                selected_collumn = int(math.floor(posX/SQUARESIZE))
+
+                if is_valid_location(board, selected_collumn):
+                    row = get_next_open_row(board, selected_collumn)
+                    drop_piece(board, row, selected_collumn, PLAYER_1_PIECE)
+                
+                    if winning_move(board, PLAYER_1_PIECE):
+                        print("PLAYER 1 WINS!!!! Congrats!!!!")
+                        game_over = True
+                
+            #Ask for Player 2 Input
+            else:
+                posX = event.pos[0]
+                selected_collumn = int(math.floor(posX/SQUARESIZE))
+
+                if is_valid_location(board, selected_collumn):
+                    row = get_next_open_row(board, selected_collumn)
+                    drop_piece(board, row, selected_collumn, PLAYER_2_PIECE)
+
+                    if winning_move(board, PLAYER_2_PIECE):
+                        print("PLAYER 2 WINS!!!! Congrats!!!!")
+                        game_over = True
+            
+            print_board(board)
+            draw_board(board)
+            pygame.display.update()
+
+            turn += 1
+            turn = turn % 2
 
             #COMAND LINE VERSION
             #Ask for Player 1 Input
